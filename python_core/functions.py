f1f0d3bff3296c41c400058d7084c03863718e6a
ubernad@crossvale.com <ulisesbernad@gmail.com>
2022-07-30 07:50:13 +0200

    Creating a module and importing into Python

---
+++ python_core/functions.py
@@ -0,0 +1,9 @@
+def square(x):
+    return x * x
+
+print(square(5))
+
+def launch_missiles():
+    print("Missiles launched!")
+
+launch_missiles()
---
+++ python_core/url_open_words.py
@@ -0,0 +1,19 @@
+from  urllib.request import urlopen
+def fetch_words():
+    story = urlopen('http://sixty-north.com/c/t.txt')
+    story_words = []
+    for line in story:
+        line_words= line.decode('utf8').split()
+        for word in line_words:
+            story_words.append(word) 
+    story.close()
+    for word in story_words:
+        print(word)
+
+if __name__== "__main__":
+    fetch_words()
+
+
+# This will import our file as a module in Python.
+# The name of the module is the same as the filename. 
+import url_open_words
